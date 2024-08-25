27063e2a33941575072e8d9102f80871aa784de5
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-08-25 18:30:28 -0500

    scaffolting

--- src/engine/Sub.py
+++ src/engine/Sub.py
@@ -4,8 +4,51 @@ class Sub:
 
     all = {}
 
-    def __init__(self, log, id, foreign_id):
+    def __init__(self, log, foreign_id, id):
         self.log = log
-        self.id = id
         self.foreign_id = foreign_id
+        self.id = id
     
+    @property
+    def log(self):
+        return self._log
+    
+    @log.setter
+    def log(self, value):
+        self._log = value
+    
+    @property
+    def foreign_id(self):
+        return self._foreign_id
+    
+    @foreign_id.setter
+    def foreign_id(self, value):
+        if isinstance(value, int):
+            self._foreign_id = value
+        else:
+            raise ValueError('foreign id should be int value')
+    
+    @classmethod
+    def drop_table(cls):
+        pass
+
+    @classmethod
+    def create_table(cls):
+        pass
+
+    @classmethod
+    def save(cls):
+        pass
+
+    def create(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    @classmethod
+    def instance_from_db(cls):
+        pass
